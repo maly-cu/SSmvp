bbc4f6dbcf1c0ad9ab1ea5ac6c8b52c0391356c6
Malynda <phoebe95.cu@gmail.com>
2023-10-23 19:15:10 +0100

    Initial commit (repo creation)

---
+++ main.py
@@ -0,0 +1,5 @@
+# TODO 1: GPS with start button, pause if loc off and show dialogue box, display coord then stop
+
+# TODO 2: Plyer Fancy notification with GPS coords
+# TODO 3: Set alarm to send notification
+# TODO 4: UI/UX logo, splash screen, name & loading animation
