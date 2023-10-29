4af00cb4954cb7d1e7f5f361dbd9e201dd6f529d
Malynda <phoebe95.cu@gmail.com>
2023-10-29 17:51:12 +0100

    ** GPS - BASIC ***     contains: Just printing coords, Error message for location service

--- main.py
+++ main.py
@@ -34,6 +34,14 @@ class DemoApp(MDApp):
     gps_location = StringProperty()
     gps_status = StringProperty('Click Start to get GPS location updates')
 
+    # def on_start(self):
+    #     gps.configure(on_location=self.on_location)
+    #
+    # def on_location(self, **kwargs):
+    #     self.gps_location = '\n'.join([
+    #         '{}={}'.format(k, v) for k, v in kwargs.items()])
+
+
     def request_android_permissions(self):
         """
         Since API 23, Android requires permission to be requested at runtime. This function requests permission and handles the response via a callback.
@@ -57,8 +65,25 @@ class DemoApp(MDApp):
         request_permissions([Permission.ACCESS_COARSE_LOCATION,
                              Permission.ACCESS_FINE_LOCATION])
 
-    def build(self):
-        self.theme_cls.primary_palette = "Orange" # to set theme color for dialogue box
+    # def build(self):
+    #     self.theme_cls.primary_palette = "Orange" # to set theme color for dialogue box
+    #
+    #     try:
+    #         gps.configure(on_location=self.on_location,
+    #                       on_status=self.on_status)
+    #     except NotImplementedError:
+    #         import traceback
+    #         traceback.print_exc()
+    #         self.gps_status = 'GPS is not implemented for your platform'
+    #
+    #     if platform == "android":
+    #         print("gps.py: Android detected. Requesting permissions")
+    #         self.request_android_permissions()
+    #     main_kv = Builder.load_file("demo.kv")
+    #     return main_kv
+
+    def on_start(self):
+        self.theme_cls.primary_palette = "Orange"  # to set theme color for dialogue box
 
         try:
             gps.configure(on_location=self.on_location,
@@ -71,16 +96,27 @@ class DemoApp(MDApp):
         if platform == "android":
             print("gps.py: Android detected. Requesting permissions")
             self.request_android_permissions()
+
+            print("------------- GPS STARTING -------------")
+            gps.start(2000, 0)
+            print(self.gps_location)
+
+            gps.stop()
+            print("------------- GPS STOPPED -------------")
+            print(f"Final GPS coordinates: {self.gps_location}")
+
+        # ------------------- Build kv file -------------------
         main_kv = Builder.load_file("demo.kv")
         return main_kv
 
-    def start(self, minTime, minDistance):
-        gps.start(minTime, minDistance)
-        print(self.gps_location)
-        gps.stop()
-
-    def stop(self):
-        gps.stop()
+    # def start(self, minTime, minDistance):
+    #     gps.start(minTime, minDistance)
+    #     print("GPS STARTING")
+    #     print(self.gps_location)
+    #
+    # def stop(self):
+    #     gps.stop()
+    #     print("GPS STOPPED")
 
     @mainthread
     def on_location(self, **kwargs):
@@ -97,7 +133,7 @@ class DemoApp(MDApp):
     # Popup to tell you to turn on location for app
     def open_gps_access_popup(self):
         self.on_pause()
-        print("GPS STOPPED")
+        print("GPS ERROR")
         dialog = MDDialog(
                 title="Location Error",
                 text="App needs Location enabled to function properly",
@@ -106,15 +142,14 @@ class DemoApp(MDApp):
         dialog.pos_hint = {'center_x': .5, 'center_y': .5}
         dialog.open()
 
-
-    def on_pause(self):
-        gps.stop()
-        return True
-
-    def on_resume(self):
-        print('Button Pressed, gps resuming')
-        gps.start(1000, 0)
-        pass
+    # def on_pause(self):
+    #     gps.stop()
+    #     return True
+    #
+    # def on_resume(self):
+    #     print('Button Pressed, gps resuming')
+    #     gps.start(1000, 0)
+    #     pass
 
 
 if __name__ == '__main__':
@@ -128,7 +163,10 @@ if __name__ == '__main__':
 
 
 
-
+    #             NOT WORKING!!!
+    # 1. DIALOG BOX OK NOT WORKING, TEST EXAMPLE ALONE (POP UP WHEN START BUTTON PRESSED)
+    # 2. GPS NOT WORKING, REMOVE stop() FROM gps.start() FUNCTION
+    # 3. GET GPS ON START OF APP (CHECK SANDBURG YOUTUBE VIDEO)
 
 
 
